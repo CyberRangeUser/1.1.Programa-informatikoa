dae9ee8b06e38e0ac31ba5b8641fd200b6e9ad69
Unai <irunavarro@lhusurbil.eus>
2025-09-26 06:45:14 +0200

    hobetzen commita

--- src/ProgramaInformatikoa.java
+++ src/ProgramaInformatikoa.java
@@ -4,7 +4,7 @@ public class ProgramaInformatikoa{
         public static final String RESET = "\u001B[0m";
         String ald1= "lehengo belaunaldiaaaaaaaa";
         String ald2= "bigarren belaunaldia";
-        System.out.println(GORRIA+"1.Programa informatikoan 6 puntu desberdin daude: "+RESET);
+        System.out.println(GORRIA+"1.Programa informatikoan 6 puntu desberdin daude, eta ondo azalduko dizkizuet: "+RESET);
         System.out.print("1.puntua");
         System.out.print("\n");
         System.out.print("2.puntua");
@@ -21,6 +21,7 @@ public class ProgramaInformatikoa{
         System.out.println("1.kodea iturburu kodea deritzogu eta .java da kasu honetan");
         System.out.println("2.kodea kode objektua deritzogu eta .class fitxategia da");
         System.out.println("3.kodea kode bitarra da exekutagarria bihutzen duena da .jar bat izan daiteke edo zuenzen JVM exekutatu dezkaeen kodea da");
+
 
                 
     }
